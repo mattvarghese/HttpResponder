3d7da8a3b1eced346f61bfa4dd9320d1f562bb0a
Matt | Varghese Mathew <16229800+mattvarghese@users.noreply.github.com>
2025-08-02 19:28:05 -0500

    fixes

--- build-and-package.sh
+++ build-and-package.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e  # If any command fails (exits with a non-zero status), immediately stop the script.
 
-# 0. Dependencies
+# 1. Dependencies
 # sudo npm install -g n
 # sudo n latest
 # sudo apt install libnss3-tools mkcert docker.io docker-compose docker-buildx
 # sudo usermod -aG docker $USER
 # Once you do the last line, logout and relogin, or do: newgrp docker
 
-# 1. Setup Certificates
+# 2. Setup Certificates
 echo "🏗️  Set up Certificate..."
 # mkcert --install   # Adds a CA cert. To remove, do: mkcert uninstall
 if [ ! -d certs ]; then
@@ -19,22 +19,28 @@ else
   echo "✅ 'certs/' folder already exists. Skipping cert generation."
 fi
 
-# 2. Adjust /etc/hosts file to add line for httplogger.local
+# 3. Make ./data folder, which is mapped to container's /app/data
+# If we don't do this and it gets auto-created, it belongs to root, and npm run clean can't delete it
+if [ ! -d data ]; then
+  mkdir -p data
+fi
+
+# 4. Adjust /etc/hosts file to add line for httplogger.local
 # echo "🔐 Checking hosts entry..."
 # if ! grep -q "httplogger.local" /etc/hosts; then
 #   echo "🧩 Adding httplogger.local to /etc/hosts"
 #   echo "127.0.0.1 httplogger.local" | sudo tee -a /etc/hosts > /dev/null
 # fi
 
-# 3. Publish backend
+# 5. Publish backend
 echo "🛠️  Publishing backend..."
 dotnet publish HttpLogger.Server/HttpLogger.Server.csproj -c Release -o ./publish
 
-# 4. Build docker image
+# 6. Build docker image
 echo "🐳 Building Docker image..."
 DOCKER_BUILDKIT=1 docker-compose build
 
-# 5. Inform user we're ready to go
+# 7. Inform user we're ready to go
 echo "✅ Ready to run with: docker-compose up"
 echo "✅ Terminate with: CTRL+C"
 echo "✅ Or run in background with: docker-compose up -d"
